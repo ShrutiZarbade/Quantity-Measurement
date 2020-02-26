aa6892fd625c0b10f00dadbfdf7c4c4ee76f196c
Shruti Zarbade <shrutizarbade22@gmail.com>
2020-02-26 19:10:57 +0530

    added:testcase-1.3 to check type

--- test/QuantityMeasurementTest.java
+++ test/QuantityMeasurementTest.java
@@ -6,14 +6,21 @@ public class QuantityMeasurementTest {
     QuantityMeasurement qm = new QuantityMeasurement();
 
     @Test
-    public void Testfeetgivenzero() throws Exception {
+    public void TestFeetGivenZero() throws Exception {
         Object result = qm.feet(0);
         assertEquals(0, (int)result);
     }
 
     @Test
-    public void Testchecknull() throws Exception {
+    public void TestCheckNull() throws Exception {
         Object result = qm.feet(null);
         assertEquals("Null Value",(String) result);
+    }
+
+    @Test
+    public void TestToCheckType() throws Exception {
+        Object result = qm.feet(1);
+        boolean b= Integer.class.isInstance(result);
+        assertEquals(true,b);
     }
 }
